ac7b0881caad00efab397891aa0ab3b975e3854a
Marcelo F. Terenciani <marcelo.terenciani@gmail.com>
2024-07-23 15:57:29 -0300

    Projeto base

---
+++ src/test/java/imposto/renda/TesteImposto.java
@@ -0,0 +1,15 @@
+package imposto.renda;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+public class TesteImposto {
+
+	@Test
+	public void testa() throws IOException {
+
+	}
+}
